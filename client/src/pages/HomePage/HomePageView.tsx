acffb64443e54a1d181255aebe144326fdf5defc
MIButcher <Butcher.i.matt@gmail.com>
2025-09-02 15:41:13 -0500

    Homepage updated for Deploy

--- client/src/pages/HomePage/HomePageView.tsx
+++ client/src/pages/HomePage/HomePageView.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from './HomePageView.module.scss';
 
+
 const HomePage: React.FC = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate('/login');
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, [navigate]);
+
     return (
 		<div className={styles.mainContainer}>
 			<div className={styles.header}>Welcome to Grocery Getter</div>
