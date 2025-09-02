b7860482e5c9152d221b6dae2e51d526177bfa29
MIButcher <Butcher.i.matt@gmail.com>
2025-09-02 15:26:59 -0500

    Deploy updates
    frontend

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
 	<React.StrictMode>
-		<BrowserRouter  basename="/client">
+		<BrowserRouter basename="/">
 			<AppRoutes />
 		</BrowserRouter>
 	</React.StrictMode>
