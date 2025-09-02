d8eb4b52c4a4f2dd2f6a67f28eeb07e490fdb790
MIButcher <Butcher.i.matt@gmail.com>
2025-09-02 14:44:32 -0500

    Deploy updates 6
    frontend

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,10 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/client/',
+  build: {
+    outDir: 'dist',
+  },
   plugins: [react()],
   cacheDir: ".vite",
   resolve: {
